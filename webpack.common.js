1bf99563a58e153bbbda2b698c6bb1813d0e69cb
Tarnovskii <artem3dgame@gmail.com>
2020-10-30 02:53:57 +0300

    init configuration

---
+++ webpack.common.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    target: 'electron-renderer',
+    entry: ['./src/index.js','./src/index.css'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules | dist/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [[
+                            '@babel/preset-env', {
+                                targets: {
+                                    esmodules: true
+                                }
+                            }],
+                            '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {}
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {modules: true}
+                    },
+                    'postcss-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
