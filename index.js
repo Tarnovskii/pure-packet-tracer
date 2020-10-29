f1c09bf241cc2914deb9812d2eebf94029234208
Tarnovskii <artem3dgame@gmail.com>
2020-10-30 02:55:58 +0300

    init entry-points

---
+++ index.js
@@ -0,0 +1,19 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+const CreateWindow = () => {
+	const window = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		backgroundColor: "white"
+	})
+
+	window.loadFile(path.join(__dirname, 'dist', 'index.html'))
+}
+
+require('electron-reload')(__dirname, {
+	electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+})
+
+
+app.whenReady().then(CreateWindow);
